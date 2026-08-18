202707aa291091c8346945d06b8c955d175f483f
KingaTshering10 <kinstsring10@gmail.com>
2026-08-18 22:52:49 +0600

    changed to 35000

--- src/app/AnimatedStats.tsx
+++ src/app/AnimatedStats.tsx
@@ -47,7 +47,7 @@ export function AnimatedStats() {
     <div className="grid grid-cols-3 gap-4">
       <Counter end={45} suffix="+" label="Farmers connected" />
       <Counter end={20} suffix="+" label="Institutional buyers" />
-      <Counter end={120000} suffix=" kg" label="Produce moved" />
+      <Counter end={35000} suffix=" kg" label="Produce moved" />
     </div>
   );
 }
